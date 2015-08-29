3acf21df6ae5fdfca24a8a2e9d56edc24705af2b
Librae <librae8226@gmail.com>
2015-08-29 12:46:05 +0800

    unify io name

--- scr/rc_gpio.lua
+++ scr/rc_gpio.lua
@@ -1,13 +1,13 @@
 print("config gpio")
 gpiomap = {[0]=3,[1]=10,[2]=4,[3]=9,[4]=1,[5]=2,[9]=11,[10]=12,[12]=6,[13]=7,[14]=5,[15]=8,[16]=0}
 
-led_b = gpiomap[13]
-gpio.mode(led_b, gpio.OUTPUT)
+io_ledb = gpiomap[13]
+gpio.mode(io_ledb, gpio.OUTPUT)
 
 io_but = gpiomap[0]
 gpio.mode(io_but, gpio.INPUT)
 
-print("led_b: "..led_b)
+print("io_ledb: "..io_ledb)
 print("io_but: "..io_but)
 
 -- check button status
@@ -18,9 +18,9 @@ flag_dsleep = 0
 
 print("checking button...")
 for i = 0, 1, 1 do
-	gpio.write(led_b, gpio.LOW)
+	gpio.write(io_ledb, gpio.LOW)
 	tmr.delay(250000)
-	gpio.write(led_b, gpio.HIGH)
+	gpio.write(io_ledb, gpio.HIGH)
 	tmr.delay(250000)
 	r[i] = gpio.read(io_but)
 end
