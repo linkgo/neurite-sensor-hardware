6154905e4925c743bb1887fb9c2b5d801c002c44
Librae <librae8226@gmail.com>
2015-08-29 12:02:00 +0800

    update sleep control

--- scr/rc_gpio.lua
+++ scr/rc_gpio.lua
@@ -1,15 +1,8 @@
-print("config gpio...")
+print("config gpio")
 gpiomap = {[0]=3,[1]=10,[2]=4,[3]=9,[4]=1,[5]=2,[9]=11,[10]=12,[12]=6,[13]=7,[14]=5,[15]=8,[16]=0}
 
 led_b = gpiomap[13]
 gpio.mode(led_b, gpio.OUTPUT)
-
-for i = 1, 2, 1 do
-	gpio.write(led_b, gpio.LOW)
-	tmr.delay(250000)
-	gpio.write(led_b, gpio.HIGH)
-	tmr.delay(250000)
-end
 
 io_but = gpiomap[0]
 gpio.mode(io_but, gpio.INPUT)
@@ -17,4 +10,24 @@ gpio.mode(io_but, gpio.INPUT)
 print("led_b: "..led_b)
 print("io_but: "..io_but)
 
-print("  [ok]")
+-- check button status
+
+local r = {[0] = 0, [1] = 0}
+
+flag_dsleep = 0
+
+print("checking button...")
+for i = 0, 1, 1 do
+	gpio.write(led_b, gpio.LOW)
+	tmr.delay(250000)
+	gpio.write(led_b, gpio.HIGH)
+	tmr.delay(250000)
+	r[i] = gpio.read(io_but)
+end
+
+if r[0] == 1 and r[1] == 1 then
+	print("no button press, enter deep sleep after work")
+	flag_dsleep = 1
+else
+	print("button presssed, enter active")
+end
--- scr/work.lua
+++ scr/work.lua
@@ -4,5 +4,5 @@ print("job done")
 
 if flag_dsleep == 1 then
 	print("enter dsleep in 3 sec, wake up after 60 sec")
-	tmr.alarm(5, 3000, 0, function() node.dsleep(60000000) end)
+	tmr.alarm(5, 3000, 0, function() print("dsleep") node.dsleep(60000000) end)
 end
