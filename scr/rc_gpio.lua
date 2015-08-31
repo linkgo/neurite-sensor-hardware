1df46ec0ee640c41f776bcc83efc8a8dd3db59ee
Librae <librae8226@gmail.com>
2015-08-31 22:17:48 +0800

    unify led control, use pwm

--- scr/rc_gpio.lua
+++ scr/rc_gpio.lua
@@ -2,7 +2,6 @@ print("# config gpio")
 gpiomap = {[0]=3,[1]=10,[2]=4,[3]=9,[4]=1,[5]=2,[9]=11,[10]=12,[12]=6,[13]=7,[14]=5,[15]=8,[16]=0}
 
 io_ledb = gpiomap[13]
-gpio.mode(io_ledb, gpio.OUTPUT)
 pwm.setup(io_ledb, 100, 512)
 pwm.start(io_ledb)
 function led(duty)
@@ -20,9 +19,9 @@ local r = 0
 flag_dsleep = false
 
 print("check button in 0.5s")
-gpio.write(io_ledb, gpio.LOW)
+led(512)
 tmr.delay(500000)
-gpio.write(io_ledb, gpio.HIGH)
+led(1023)
 r = gpio.read(io_but)
 
 if file.open("config_boot.lc") then
