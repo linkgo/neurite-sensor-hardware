a68b84463c3479eac012e317b1d0bf9b06f45bbf
Librae <librae8226@gmail.com>
2015-08-31 11:40:06 +0800

    lower cpu usage on led

--- scr/work.lua
+++ scr/work.lua
@@ -6,17 +6,17 @@ function led(duty)
 end
 local led_duty = 1023
 local led_state = 0
-tmr.alarm(tmr_led, 10, 1, function()
-	if led_duty > 1020 then
+tmr.alarm(tmr_led, 50, 1, function()
+	if led_duty > 1010 then
 		led_state = 1
 	elseif led_duty < 512 then
 		led_state = 0
 	else
 	end
 	if led_state == 0 then
-		led_duty = led_duty + 2
+		led_duty = led_duty + 10
 	else
-		led_duty = led_duty - 2
+		led_duty = led_duty - 10
 	end
 	led(led_duty)
 end)
