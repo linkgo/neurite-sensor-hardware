161680f178591204aae852700a651d3a182f05a4
Librae <librae8226@gmail.com>
2015-08-31 23:14:41 +0800

    add compiled tsl2561 binary

--- scr/tsl.lua
+++ scr/tsl.lua
@@ -1,14 +1,14 @@
-tmr.alarm(0, 5000, 1, function()
-
+tmr.alarm(0, 1000, 1, function()
     -- init module
     tsl2561 = require("tsl2561")
     tsl2561.init(i2c_sda, i2c_scl)
 
     -- read value
     l = tsl2561.readVisibleLux()
+    print("lux: "..l.." lx")
 
     -- release module
     tsl2561 = nil
     package.loaded["tsl2561"]=nil
-
+    collectgarbage()
 end)
--- scr/tsl2561.lua
+++ scr/tsl2561.lua
@@ -14,6 +14,58 @@ local CH_SCALE      = 10       -- scale channel values by 2^10
 local CHSCALE_TINT0 = 0x7517   -- 322/11 * 2^CH_SCALE
 local CHSCALE_TINT1 = 0x0fe7   -- 322/81 * 2^CH_SCALE
 
+-- Scale table
+local S = {}
+S.K1T = 0x0040   -- 0.125 * 2^RATIO_SCALE
+S.B1T = 0x01f2   -- 0.0304 * 2^LUX_SCALE
+S.M1T = 0x01be   -- 0.0272 * 2^LUX_SCALE
+S.K2T = 0x0080   -- 0.250 * 2^RATIO_SCA
+S.B2T = 0x0214   -- 0.0325 * 2^LUX_SCALE
+S.M2T = 0x02d1   -- 0.0440 * 2^LUX_SCALE
+S.K3T = 0x00c0   -- 0.375 * 2^RATIO_SCALE
+S.B3T = 0x023f   -- 0.0351 * 2^LUX_SCALE
+S.M3T = 0x037b   -- 0.0544 * 2^LUX_SCALE
+S.K4T = 0x0100   -- 0.50 * 2^RATIO_SCALE
+S.B4T = 0x0270   -- 0.0381 * 2^LUX_SCALE
+S.M4T = 0x03fe   -- 0.0624 * 2^LUX_SCALE
+S.K5T = 0x0138   -- 0.61 * 2^RATIO_SCALE
+S.B5T = 0x016f   -- 0.0224 * 2^LUX_SCALE
+S.M5T = 0x01fc   -- 0.0310 * 2^LUX_SCALE
+S.K6T = 0x019a   -- 0.80 * 2^RATIO_SCALE
+S.B6T = 0x00d2   -- 0.0128 * 2^LUX_SCALE
+S.M6T = 0x00fb   -- 0.0153 * 2^LUX_SCALE
+S.K7T = 0x029a   -- 1.3 * 2^RATIO_SCALE
+S.B7T = 0x0018   -- 0.00146 * 2^LUX_SCALE
+S.M7T = 0x0012   -- 0.00112 * 2^LUX_SCALE
+S.K8T = 0x029a   -- 1.3 * 2^RATIO_SCALE
+S.B8T = 0x0000   -- 0.000 * 2^LUX_SCALE
+S.M8T = 0x0000   -- 0.000 * 2^LUX_SCALE
+
+S.K1C = 0x0043   -- 0.130 * 2^RATIO_SCALE
+S.B1C = 0x0204   -- 0.0315 * 2^LUX_SCALE
+S.M1C = 0x01ad   -- 0.0262 * 2^LUX_SCALE
+S.K2C = 0x0085   -- 0.260 * 2^RATIO_SCALE
+S.B2C = 0x0228   -- 0.0337 * 2^LUX_SCALE
+S.M2C = 0x02c1   -- 0.0430 * 2^LUX_SCALE
+S.K3C = 0x00c8   -- 0.390 * 2^RATIO_SCALE
+S.B3C = 0x0253   -- 0.0363 * 2^LUX_SCALE
+S.M3C = 0x0363   -- 0.0529 * 2^LUX_SCALE
+S.K4C = 0x010a   -- 0.520 * 2^RATIO_SCALE
+S.B4C = 0x0282   -- 0.0392 * 2^LUX_SCALE
+S.M4C = 0x03df   -- 0.0605 * 2^LUX_SCALE
+S.K5C = 0x014d   -- 0.65 * 2^RATIO_SCALE
+S.B5C = 0x0177   -- 0.0229 * 2^LUX_SCALE
+S.M5C = 0x01dd   -- 0.0291 * 2^LUX_SCALE
+S.K6C = 0x019a   -- 0.80 * 2^RATIO_SCALE
+S.B6C = 0x0101   -- 0.0157 * 2^LUX_SCALE
+S.M6C = 0x0127   -- 0.0180 * 2^LUX_SCALE
+S.K7C = 0x029a   -- 1.3 * 2^RATIO_SCALE
+S.B7C = 0x0037   -- 0.00338 * 2^LUX_SCALE
+S.M7C = 0x002b   -- 0.00260 * 2^LUX_SCALE
+S.K8C = 0x029a   -- 1.3 * 2^RATIO_SCALE
+S.B8C = 0x0000   -- 0.000 * 2^LUX_SCALE
+S.M8C = 0x0000   -- 0.000 * 2^LUX_SCALE
+
 local moduleName = 0
 local M = {}
 _G[moduleName] = M
@@ -90,8 +142,84 @@ function M.readVisibleLux()
    if(ch0/ch1 < 2 and ch0 > 4900) then
      return -1  -- ch0 out of range, but ch1 not. the lux is not valid in this situation.
    end
-     print("ch0: "..ch0.." ch1: "..ch1)
-     return 0
+   return M.calculateLux(0, 0, 0)  -- T package, no gain, 13ms
+end
+
+function M.calculateLux(iGain, tInt, iType)
+   if tInt == 0 then -- 13.7 msec
+      chScale = CHSCALE_TINT0
+   elseif tInt == 1 then -- 101 msec
+      chScale = CHSCALE_TINT1
+   else -- assume no scaling
+      chScale = bit.lshift(1,CH_SCALE)
+   end
+
+   if (not iGain) then chScale = bit.lshift(chScale,4) end -- scale 1X to 16X
+   -- scale the channel values
+   channel0 = bit.rshift((ch0 * chScale),CH_SCALE)
+   channel1 = bit.rshift((ch1 * chScale),CH_SCALE)
+
+   ratio1 = 0
+   if channel0 ~= 0 then ratio1 = bit.lshift(channel1,(RATIO_SCALE+1))/channel0 end
+   -- round the ratio value
+   ratio = bit.rshift((ratio1 + 1),1)
+
+   if iType == 0 then -- T package
+       if ratio >= 0 and ratio <= S.K1T then
+          b=S.B1T
+          m=S.M1T
+       elseif ratio <= S.K2T then
+          b=S.B2T
+          m=S.M2T
+       elseif ratio <= S.K3T then
+          b=S.B3T
+          m=S.M3T
+       elseif ratio <= S.K4T then
+          b=S.B4T
+          m=S.M4T
+       elseif ratio <= S.K5T then
+          b=S.B5T
+          m=S.M5T
+       elseif ratio <= S.K6T then
+          b=S.B6T
+          m=S.M6T
+       elseif ratio <= S.K7T then
+          b=S.B7T
+          m=S.M7T
+       elseif ratio > S.K8T then
+          b=S.B8T
+          m=S.M8T
+       end
+   elseif iType == 1 then -- CS package
+       if ratio >= 0 and ratio <= S.K1C then
+          b=S.B1C
+          m=S.M1C
+       elseif ratio <= S.K2C then
+          b=S.B2C
+          m=S.M2C
+       elseif ratio <= S.K3C then
+          b=S.B3C
+          m=S.M3C
+       elseif ratio <= S.K4C then
+          b=S.B4C
+          m=S.M4C
+       elseif ratio <= S.K5C then
+          b=S.B5C
+          m=S.M5C
+       elseif ratio <= S.K6C then
+          b=S.B6C
+          m=S.M6C
+       elseif ratio <= S.K7C then
+          b=S.B7C
+          m=S.M7C
+       end
+   end
+   temp=((channel0*b)-(channel1*m))
+   if temp<0 then temp=0 end
+   temp = temp + bit.lshift(1,(LUX_SCALE-1))
+   -- strip off fractional portion
+   lux = bit.rshift(temp,LUX_SCALE)
+   return lux
 end
 
 return M
