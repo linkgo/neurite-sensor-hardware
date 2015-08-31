acb0ed02ea98d2f74e7fea6743d1a74323c6f0a0
Librae <librae8226@gmail.com>
2015-08-31 12:00:33 +0800

    run file like function call

--- scr/cat.lua
+++ scr/cat.lua
@@ -1,6 +1,6 @@
-local f=...
-print("args:"..f)
-
-file.open(f, r)
-print(file.read(EOF))
-file.close()
+return function(f)
+	print("args:"..f)
+	file.open(f, "r")
+	print(file.read(EOF))
+	file.close()
+end
