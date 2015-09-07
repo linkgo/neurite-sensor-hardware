8b587f11540c99ebf6a59aa2cd89d03797ea8e2e
Librae <librae8226@gmail.com>
2015-09-07 19:58:45 +0800

    update tsl2561, remove trivial calculations

--- scr/tsl.lua
+++ scr/tsl.lua
@@ -1,13 +1,8 @@
 tmr.alarm(0, 1000, 1, function()
-    -- init module
     tsl2561 = require("tsl2561")
-    tsl2561.init(i2c_sda, i2c_scl)
-
-    -- read value
-    l = tsl2561.readVisibleLux()
-    print("lux: "..l.." lx")
-
-    -- release module
+    tsl2561.init()
+    local ch0, ch1 = tsl2561.readRaw()
+    print("0: "..ch0.." 1: "..ch1)
     tsl2561 = nil
     package.loaded["tsl2561"]=nil
     collectgarbage()
--- scr/tsl2561.lua
+++ scr/tsl2561.lua
@@ -18,38 +18,36 @@ _G[moduleName] = M
 local id = 0
 
 -- local vars
-local ch0,ch1 = 0
+local ch0, ch1 = 0
 
 function M.getLux()
-    local CH0_LOW=readRegister(TSL2561_Address,TSL2561_Channel0L)
-    local CH0_HIGH=readRegister(TSL2561_Address,TSL2561_Channel0H)
+    local CH0_LOW=readRegister(TSL2561_Address, TSL2561_Channel0L)
+    local CH0_HIGH=readRegister(TSL2561_Address, TSL2561_Channel0H)
     --read two bytes from registers 0x0E and 0x0F
-    local CH1_LOW=readRegister(TSL2561_Address,TSL2561_Channel1L)
-    local CH1_HIGH=readRegister(TSL2561_Address,TSL2561_Channel1H)
+    local CH1_LOW=readRegister(TSL2561_Address, TSL2561_Channel1L)
+    local CH1_HIGH=readRegister(TSL2561_Address, TSL2561_Channel1H)
 
-    ch0 = bit.bor(bit.lshift(CH0_HIGH,8),CH0_LOW)
-    ch1 = bit.bor(bit.lshift(CH1_HIGH,8),CH1_LOW)
+    ch0 = bit.bor(bit.lshift(CH0_HIGH,8), CH0_LOW)
+    ch1 = bit.bor(bit.lshift(CH1_HIGH,8), CH1_LOW)
 end
 
-function M.init(sda, scl)
-   i2c.setup(id, sda, scl, i2c.SLOW)
-   writeRegister(TSL2561_Address,TSL2561_Control,0x03)  -- POWER UP
-   writeRegister(TSL2561_Address,TSL2561_Timing,0x00)  --No High Gain (1x), integration time of 13ms
-   writeRegister(TSL2561_Address,TSL2561_Interrupt,0x00)
-   writeRegister(TSL2561_Address,TSL2561_Control,0x00)  -- POWER Down
+function M.init()
+   writeRegister(TSL2561_Address, TSL2561_Control, 0x03)  -- POWER UP
+   writeRegister(TSL2561_Address, TSL2561_Timing, 0x00)  --No High Gain (1x), integration time of 13ms
+   writeRegister(TSL2561_Address, TSL2561_Interrupt, 0x00)
+   writeRegister(TSL2561_Address, TSL2561_Control, 0x00)  -- POWER Down
 end
 
-function M.readVisibleLux()
-   writeRegister(TSL2561_Address,TSL2561_Control,0x03)  -- POWER UP
+function M.readRaw()
+   writeRegister(TSL2561_Address, TSL2561_Control, 0x03)  -- POWER UP
    tmr.delay(14000)
    M.getLux()
 
-   writeRegister(TSL2561_Address,TSL2561_Control,0x00)  -- POWER Down
+   writeRegister(TSL2561_Address, TSL2561_Control, 0x00)  -- POWER Down
    if(ch0/ch1 < 2 and ch0 > 4900) then
-     return -1  -- ch0 out of range, but ch1 not. the lux is not valid in this situation.
+     return -1, -1
    end
-   print("ch0: "..ch0.." ch1: "..ch1)
-   return 0
+   return ch0, ch1
 end
 
 return M
