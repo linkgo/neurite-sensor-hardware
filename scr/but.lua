f317effc7650595507de649a0103df82332df240
Librae <librae8226@gmail.com>
2015-09-10 21:09:35 +0800

    prints can't be in tmr callback, but is ok now

--- scr/but.lua
+++ scr/but.lua
@@ -1,5 +1,3 @@
-print("button")
-
 local value = true
 local t = 0
 local dt = 0
@@ -9,22 +7,21 @@ local r_prev = 1
 function but_cb(dt)
 	value = not value
 	if value == false then
-		print("low")
+		led(1023)
 	else
-		print("high")
+		led(512)
 	end
 end
 
 tmr.alarm(tmr_but, 50, 1, function()
-	local r_curr = gpio.read(io_but)
+	r_curr = gpio.read(io_but)
 	if r_curr == 0 and r_prev == 1 then
 		t = tmr.now()
 		r_prev = r_curr
 	elseif r_curr == 1 and r_prev == 0 then
 		dt = tmr.now() - t
-		print("dt: "..dt)
 		r_prev = r_curr
-		if (dt > 139999) then but_cb(dt) end
+		if (dt > 89999) then but_cb(dt) end
 	else
 		-- do nothing
 	end
